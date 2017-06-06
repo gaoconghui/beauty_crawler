7d010f695f8108905a924a3d09de3536fd3f9158
wza8652wza <381147882@qq.com>
2017-06-06 23:04:10 +0800

    增加mt.91.com美女图种子

---
+++ seed.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+种子页面，因为涉及到tag以及一些自定义的标签，需要人工编辑
+导入mongo中进行调度
+
+_id 格式 : domain___listid
+
+"""
+
+seeds = [
+    # mt.91.com
+    {
+        "_id": "mt.91.com___meinv/xiangchemeinv/list_29",
+        "domain": "mt.91.com",
+        "extends": {
+            "tags": ["香车美女"]
+        }
+    },
+    {
+        "_id": "mt.91.com___meinv/gudianmeinv/list_30",
+        "domain": "mt.91.com",
+        "extends": {
+            "tags": ["古典美女"]
+        }
+    },
+    {
+        "_id": "mt.91.com___meinv/relaxiezhen/list_31",
+        "domain": "mt.91.com",
+        "extends": {
+            "tags": ["热辣写真"]
+        }
+    },
+    {
+        "_id": "mt.91.com___meinv/xingganjiaren/list_32",
+        "domain": "mt.91.com",
+        "extends": {
+            "tags": ["性感佳人"]
+        }
+    },
+    {
+        "_id": "mt.91.com___meinv/guanggaomeitu/list_56",
+        "domain": "mt.91.com",
+        "extends": {
+            "tags": ["广告美图"]
+        }
+    },
+    {
+        "_id": "mt.91.com___meinv/mingxingxiezhen/list_57",
+        "domain": "mt.91.com",
+        "extends": {
+            "tags": ["明星写真"]
+        }
+    },
+]
